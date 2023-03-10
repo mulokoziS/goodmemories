57d88b75f2ba78b74885f0d5ba9f1276452fa179
mulokoziS <mulokoziscalion@gmail.com>
2023-03-10 12:35:26 +0300

    all files

--- conn.php
+++ conn.php
@@ -2,6 +2,6 @@
 $dbhost = "sql958.main-hosting.eu";
 $dbuser = "u554817690_jambiA";
 $dbpass = "MzeeWaJambiA@2023";
-$db = "u554817690_gmtz";
+$db = "u554817690_gmt";
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error)
 ?>
